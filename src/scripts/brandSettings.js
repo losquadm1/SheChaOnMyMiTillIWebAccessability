97d37788838e1f20ab2ec1fa6d637195e05e06fe
losquadm1 <losquadm1@newpaltz.edu>
2026-03-11 06:58:59 +0000

    Did project

--- src/scripts/brandSettings.js
+++ src/scripts/brandSettings.js
@@ -5,28 +5,28 @@ export const brandSettings = {
         variations: [ // you might have wordmarks / different color variants than what is listed here, that's fine just adjust it
             { 
                 name: "Primary - Light Background", 
-                src: "public/images/PurpleLogo.png", // Add image path
+                src: "/images/PurpleLogo.png", // Add image path
                 bg: "#ffffff", 
                 darkText: true,
                 description: "Use this version on white or light backgrounds."
             },
             { 
                 name: "Primary - Dark Background", 
-                src: "public/images/PurpleLogo.png", // Add image path
+                src: "/images/PurpleLogo.png", // Add image path
                 bg: "#333333", 
                 darkText: false,
                 description: "Use this version on dark or brand-colored backgrounds."
             },
             { 
                 name: "Monochrome - Black", 
-                src: "public/images/BlackLogo.png", // Add image path
+                src: "/images/BlackLogo.png", // Add image path
                 bg: "#ffffff", 
                 darkText: true,
                 description: "For black and white print or single-color applications."
             },
             { 
                 name: "Monochrome - White", 
-                src: "public/images/WhiteLogo.png", // Add image path
+                src: "/images/WhiteLogo.png", // Add image path
                 bg: "#000000", 
                 darkText: false,
                 description: "For dark backgrounds where color is not permitted."
@@ -35,8 +35,8 @@ export const brandSettings = {
     },
     favicon: "/favicon.svg", // Add favicon URL here
     typography: {
-        primaryFont: "Helvetica Neue",  // this will automatically get set as the page font
-        secondaryFont: "Verdana", // dont be a bum, go find some google/adobe fonts you like. just make sure to load them in the stylesheet or Layout astro file 
+        primaryFont: "Satoshi",  // this will automatically get set as the page font
+        secondaryFont: "Archivo Black", // dont be a bum, go find some google/adobe fonts you like. just make sure to load them in the stylesheet or Layout astro file 
         baseSize: "16px",
         lineHeight: "1.5",
         paragraphSpacing: "2"
@@ -48,6 +48,6 @@ export const brandSettings = {
         { name: "Chami Purple", hex: "#7653a2" },
         { name: "Read/Unselected Off-White", hex: "#A19FAD" },
         { name: "Link Pink", hex: "#e2c8e1" },
-        { name: "Light Gray", hex: "#ed1f24" }
+        { name: "Notification Red", hex: "#ed1f24" } 
     ]
 };
